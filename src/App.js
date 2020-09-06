1ac4d0bf4e25162591be6c7bd7209d2bf9424917
balraaz15 <balraaz11@gmail.com>
2020-09-07 00:47:22 +0545

    added header component and started on user signin and signup

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./component/header/header.component";
+import UserEntry from "./pages/user-entry/user-entry.component";
 
 function App() {
   return (
     <div className="App">
-      <h1>Sports Shop</h1>
+      <Header />
       <Switch>
         <Route path="/shop" component={ShopPage} />
+        <Route path="/entry" component={UserEntry} />
         <Route path="/" component={HomePage} />
       </Switch>
       {/* <HomePage /> */}
