3734e9eeefa3aeca014b6969552ea532742b2ff3
balraaz15 <balraaz11@gmail.com>
2020-09-30 23:39:30 +0545

    implemented cart detail data after clicking add to cart from shop page

--- src/component/cart-dropdown/cart-dropdown.component.jsx
+++ src/component/cart-dropdown/cart-dropdown.component.jsx
@@ -7,16 +7,26 @@ import "./cart-dropdown.styles.scss";
 
 import CustomButton from "../custom-button/custom-button.component";
 
-const CartDropdown = ({ toggleCartHidden }) => (
+const CartDropdown = ({ toggleCartHidden, cartItems }) => (
   <div className="cart-dropdown" onMouseLeave={toggleCartHidden}>
-    <div className="cart-items"></div>
+    <div className="cart-items">
+      {cartItems.map((item) => (
+        <p key={item.id}>
+          {item.product_name} {item.price}
+        </p>
+      ))}
+    </div>
     <CustomButton>My Cart</CustomButton>
     <CustomButton>Checkout</CustomButton>
   </div>
 );
 
+const mapStateToProps = ({ cart }) => ({
+  cartItems: cart.cartItems,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartDropdown);
+export default connect(mapStateToProps, mapDispatchToProps)(CartDropdown);
--- src/component/collection-item/collection-item.component.jsx
+++ src/component/collection-item/collection-item.component.jsx
@@ -1,15 +1,29 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import CustomButton from "../custom-button/custom-button.component";
+import { addItem } from "../../redux/cart/cart-actions";
 
 import "./collection-item.styles.scss";
 
-const CollectionItem = ({ id, product_name, imageUrl, price }) => (
-  <div className="collection-item">
-    <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
-    <div className="collection-footer">
-      <span className="product-name">{product_name}</span>
-      <span className="price">{price}</span>
+const CollectionItem = ({ item, addItem }) => {
+  const { product_name, imageUrl, price } = item;
+  return (
+    <div className="collection-item">
+      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className="collection-footer">
+        <span className="product-name">{product_name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton inverted onClick={() => addItem(item)}>
+        Add To Cart
+      </CustomButton>
     </div>
-  </div>
-);
+  );
+};
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
