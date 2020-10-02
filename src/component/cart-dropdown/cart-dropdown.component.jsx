14d51d4fbf95d0ab227ce73a2f1db59c2f13c412
balraaz15 <balraaz11@gmail.com>
2020-10-02 23:45:54 +0545

    added reselect, replaced cart selectors with memoized selectors

--- src/component/cart-dropdown/cart-dropdown.component.jsx
+++ src/component/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
 import { toggleCartHidden } from "../../redux/cart/cart-actions";
 
@@ -7,25 +8,32 @@ import "./cart-dropdown.styles.scss";
 
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
+import { selectCartItems } from "../../redux/cart/cart-selectors";
 
-const CartDropdown = ({ toggleCartHidden, cartItems }) => (
+const CartDropdown = ({ toggleCartHidden, cartItems, history }) => (
   <div className="cart-dropdown" onMouseLeave={toggleCartHidden}>
     <div className="cart-items">
-      {cartItems.map((item) => (
-        <CartItem key={item.id} item={item} />
-      ))}
+      {cartItems.length ? (
+        cartItems.map((item) => <CartItem key={item.id} item={item} />)
+      ) : (
+        <h5 className="empty-cart">Your Cart is Empty!</h5>
+      )}
     </div>
     <CustomButton>My Cart</CustomButton>
-    <CustomButton>Checkout</CustomButton>
+    <CustomButton onClick={() => history.push("/checkout")}>
+      Checkout
+    </CustomButton>
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems: cartItems,
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
 });
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(CartDropdown);
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(CartDropdown)
+);
