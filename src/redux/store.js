dde847aad0539f141747c544906d7b134d437d48
balraaz15 <balraaz11@gmail.com>
2020-10-03 12:40:22 +0545

    added data persistence with redux-persist library and made cart data persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 
 import rootReducer from "./root-reducer";
 
@@ -6,6 +7,8 @@ import { logger } from "redux-logger";
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
